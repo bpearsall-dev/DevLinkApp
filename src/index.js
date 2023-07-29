7bb809af2f9a5d3881fb1305b5044c3b998e4665
bpearsall-dev <85518174+bpearsall-dev@users.noreply.github.com>
2023-07-29 17:30:16 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import 'semantic-ui-css/semantic.min.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
